df171c7c093564b3b22234e1a7f812e78f82dba7
uti1man <harshitjawla123@gmail.com>
2022-04-29 06:09:49 +0530

    Data form to server

--- src/components/admin/createEvent.js
+++ src/components/admin/createEvent.js
@@ -1,35 +1,108 @@
-import { Checkbox } from "fomantic-ui-react"
+import { Checkbox, Button } from "fomantic-ui-react"
+// import AddUpdate from "../../../server/routes/update";
+
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+
+import * as Realm from "realm-web";
+const {
+  BSON: { ObjectId },
+} = Realm;
+
+const APP_ID = 'application-0-lkkyg';
+const app = new Realm.App({ id: APP_ID });
+
+async function okDone(form) {
+  const mongo = app.currentUser.mongoClient('mongodb-atlas');
+  const collection = mongo.db('events').collection('geu');
+  let theEvent = {
+    eventName: form.eventName,
+    imageLink: form.imageLink,
+    eventLink: form.eventLink,
+    forDeemed: form.forDeemed,
+    forHill: form.forHill,
+  }
+  const result = await collection.updateOne({ eventName: form.eventName }, { $setOnInsert: theEvent }, { upsert: true });
+  console.log('Done'+result);
+}
+
+function pokDone(form) {
+  let theEvent = {
+    eventName: form.eventName,
+    imageLink: form.imageLink,
+    eventLink: form.eventLink,
+    forDeemed: form.forDeemed,
+    forHill: form.forHill,
+  }
+  console.log(theEvent);
+}
 
 export default function CreateEvent() {
-    return (
-        <div className="ss">
-            <h1>Create New Event</h1>
-            <br />
-            <form className="ui form">
-                <div className="field">
-                    <label>Event Name</label>
-                    <input type="text" name="first-name" placeholder="Rogathon 2.0" />
-                </div>
-                <div className="field">
-                    <label>Event banner link</label>
-                    <input type="text" name="last-name" placeholder="https://imgur.com/wjfi933" />
-                </div><br />
-                <div className="field">
-                    <div className="inline field">
-                        <div className="ui checkbox">
-                            <Checkbox label='Deemed' /><br /><br />
-                            <Checkbox label='Hill' />
-                        </div>
-                    </div>
-                    <br /><br />
-                    <div className="ui toggle checkbox">
-                        <Checkbox toggle label={<div>I agree to the <a href="https://termsandconditions.com" target="blank">Terms and Conditions</a></div>} />
-                        {/* <label></label> */}
-                    </div>
-                    <br /><br />
-                </div>
-                <button className="ui button" type="submit">Submit</button>
-            </form>
+  const [form, setForm] = useState({
+    eventName: "",
+    imageLink: "",
+    eventLink: "",
+    forDeemed: true,
+    forHill: false,
+    tnc: false,
+  });
+
+  function updateForm(value) {
+    return setForm((prev) => {
+      return { ...prev, ...value };
+    });
+  }
+
+  return (
+    <div className="ss">
+      <h1>Create New Event</h1>
+      <br />
+      <form onSubmit={function (e) { e.preventDefault(); okDone(form) }} className="ui form">
+        <div className="field">
+          <label>Event Name</label>
+          <input type="text" name="eventName" placeholder="Rogathon 2.0"
+            value={form.eventName}
+            onChange={(e) => updateForm({ eventName: e.target.value })} />
         </div>
-    )
+        <div className="field">
+          <label>Event banner link</label>
+          <input type="text" name="imageLink" placeholder="https://imgur.com/wjfi933"
+            value={form.imageLink}
+            onChange={(e) => updateForm({ imageLink: e.target.value })} />
+        </div>
+        <div className="field">
+          <label>Event Registration link</label>
+          <input type="text" name="eventLink" placeholder="https://forms.google.com/sd3dk-43f34-er443"
+            value={form.eventLink}
+            onChange={(e) => updateForm({ eventLink: e.target.value })} />
+        </div><br />
+        <div className="field">
+          <div className="inline field">
+            <label>Deemed</label>
+            <input
+              name="forDeemed"
+              checked={form.forDeemed}
+              onChange={(e) => updateForm({ forDeemed: e.target.checked })}
+              type='checkbox'
+            />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <label>Hill</label>&nbsp;&nbsp;&nbsp;&nbsp;
+            <input
+              name="forHill"
+              checked={form.forHill}
+              onChange={(e) => updateForm({ forHill: e.target.checked })}
+              type='checkbox'
+            />
+            <br /><br />
+          </div>
+          <div className="ui toggle checkbox">
+            <Checkbox readOnly checked={true} label={<div>By proceeding, you agree to our <a href="https://termsandconditions.com" target="blank">Terms and Conditions</a></div>} />
+            {/* <label></label> */}
+          </div>
+          <br /><br />
+        </div>
+        <button className="ui button" type="submit">Submit</button>
+      </form>
+    </div>
+  )
 }
